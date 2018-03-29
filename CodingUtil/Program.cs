1883aaca90fe73f3d153b6a03bfc0a4d5e75a9fa
Victor Martin <victormartin@centralite.com>
2018-03-29 15:52:19 -0500

    Add project files.

---
+++ CodingUtil/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingUtil
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
