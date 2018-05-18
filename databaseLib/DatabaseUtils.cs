c78403c2388b5c3f1e8a712115ba1f78e5d17b54
Victor Martin <victormartin@centralite.com>
2018-05-18 10:10:40 -0500

    Log db error Changed db to v2

--- databaseLib/DatabaseUtils.cs
+++ databaseLib/DatabaseUtils.cs
@@ -24,6 +24,8 @@ namespace CodingUtil
                     // Get production site
                     string mac = StationSetupUtility.GetMacAddress();
                     var production_stie_id = cx.StationSites.Where(s => s.StationMac == mac).Single().ProductionSiteId;
+                    if (production_stie_id == 0)
+                        _logger.Warn($"Invalid production site id: {production_stie_id} for MAC: {mac}");
 
                     // db eui is a string
                     string euistr = eui.ToString("X16");
@@ -33,18 +35,24 @@ namespace CodingUtil
                     
                     if(q.Any())
                     {
+                        return q.Single().Id;
+
+                        // I'm not sure we care what site previously coded the device
+                        // So I'm removing this for now...
+                        // Checking for a valid site id maybe better...
+
                         // Check is the same site id
-                        int db_site_id = q.Single().ProductionSiteId;
-                        if (db_site_id != production_stie_id)
-                        {
-                            string msg = $"EUI {euistr} already in db with site id = {db_site_id}, this machine is assigned site id {production_stie_id}";
-                            _logger.Error(msg);
-                            throw new Exception(msg);
-                        }
-                        else
-                        {
-                            return q.Single().Id;
-                        }
+                        //int db_site_id = q.Single().ProductionSiteId;
+                        //if (db_site_id != production_stie_id)
+                        //{
+                        //    string msg = $"EUI {euistr} already in db with site id = {db_site_id}, this machine is assigned site id {production_stie_id}";
+                        //    _logger.Error(msg);
+                        //    throw new Exception(msg);
+                        //}
+                        //else
+                        //{
+                        //    return q.Single().Id;
+                        //}
                     }
 
 
@@ -61,13 +69,17 @@ namespace CodingUtil
             }
             catch (Exception ex)
             {
+                _logger.Error($"Exception in InsertEUI: {ex.Message}\r\n{ex.StackTrace}");
+
                 if (ex.GetBaseException().GetType() == typeof(SqlException))
                 {
+                    // I think I originally added this to skip errors when trying to insert already existing EUI
+                    // This should no longer happen as we now check for if already exists...So re-throw it
                     SqlException sx = (SqlException)ex.GetBaseException();
                     if (sx.Number == 2627)
                     {
                         //Violation of primary key/Unique constraint
-                        // 
+                        throw;
                     }
                     else
                     {
@@ -79,8 +91,6 @@ namespace CodingUtil
                     throw;
                 }
             }
-
-            return -1;
         }
     }
 }
